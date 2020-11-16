1e20e3c41c4d55d1493ab59b99d0537ee1cec156
hamid <hamid.ghorogchian.sf@gmail.com>
2020-11-16 14:17:00 -0800

    rename testcases

--- Memtool/stage1/tests.py
+++ Memtool/stage1/tests.py
@@ -18,37 +18,37 @@ Q_S = 'Please press "y" to see the answer or press "n" to pass:'
 
 class FlashCardTest(StageTest):
     def generate(self):
-        return [TestCase(stdin=[self.first,
-                                self.second,
-                                self.third,
-                                self.fourth,
-                                self.fifth,
-                                self.third_,
-                                self.fourth_,
-                                self.fifth_,
-                                self.sixth,
-                                self.seventh,
-                                self.eighth,
-                                self.eighth_n,
+        return [TestCase(stdin=[self.test1_input1,
+                                self.test1_input2,
+                                self.test1_input3,
+                                self.test1_input4,
+                                self.test1_input5,
+                                self.test1_input6,
+                                self.test1_input7,
+                                self.test1_input8,
+                                self.test1_input9,
+                                self.test1_input10,
+                                self.test1_input11,
+                                self.test1_input12,
                                 ])
-            , TestCase(stdin=[self.first_test_two,
-                              self.second_test_two,
-                              self.third_test_two])
+            , TestCase(stdin=[self.test2_input1,
+                              self.test2_input2,
+                              self.test2_input3])
             , TestCase(stdin=['1',
                               '4',
-                              self.third_test_three,
-                              self.fourth_test_three])
+                              self.test3_input1,
+                              self.test3_input2])
             , TestCase(stdin=['1',
                               '1',
                               ' ',
-                              self.first_test_four,
-                              self.second_test_four])
+                              self.test4_input4,
+                              self.test4_input5])
             , TestCase(stdin=['1',
                               '1',
                               'What is the capital city of Peru?',
                               ' ',
-                              self.first_test_five,
-                              self.second_test_five])]
+                              self.test5_input5,
+                              self.test5_input6])]
 
     def check_main_menu(self, out):
         main_menu_list = MAIN_MENU.strip().split('\n')
@@ -104,7 +104,7 @@ class FlashCardTest(StageTest):
             return 3
         return 4
 
-    def first(self, out):
+    def test1_input1(self, out):
         result, error, action = self.check_main_menu(out)
         if result == 1:
             return CheckResult.wrong(f'The main menu has "3" lines and it must be like this:\n{MAIN_MENU}')
@@ -113,7 +113,7 @@ class FlashCardTest(StageTest):
         if result == 3:
             return '1'
 
-    def second(self, out):
+    def test1_input2(self, out):
         result, index, sub_menue_line = self.check_sub_menu(out)
         if result == 1:
             return CheckResult.wrong(f'The sub_menu has "2" lines and it must be like this:\n{SUB_MENU}')
@@ -122,56 +122,56 @@ class FlashCardTest(StageTest):
         if result == 3:
             return '1'
 
-    def third(self, out):
+    def test1_input3(self, out):
         result = self.check_question(out)
         if result == 1:
             return CheckResult.wrong(f'the word {out} spelling is wrong, it should be like this:\nQuestion:')
         if result == 2:
             return FIRST_QUESTION
 
-    def fourth(self, out):
+    def test1_input4(self, out):
         result = self.check_answer(out)
         if result == 1:
             return CheckResult.wrong(f'the word {out} spelling is wrong it should be like this:\nAnswer:')
         if result == 2:
             return FIRST_ANSWER
 
-    def fifth(self, out):
+    def test1_input5(self, out):
         result, a, c = self.check_sub_menu(out)
         if result == 3:
             return '1'
         if result == 1 or result == 2:
             return CheckResult.wrong('after entering answer the sub_menu must be printed')
 
-    def third_(self, out):
+    def test1_input6(self, out):
         result = self.check_question(out)
         if result == 1:
             return CheckResult.wrong(f'the word {out} spelling is wrong, it should be like this:\nQuestion:')
         if result == 2:
             return SECOND_QUESTION
 
-    def fourth_(self, out):
+    def test1_input7(self, out):
         result = self.check_answer(out)
         if result == 1:
             return CheckResult.wrong(f'the word {out} spelling is wrong it should be like this:\nAnswer:')
         if result == 2:
             return SECOND_ANSWER
 
-    def fifth_(self, out):
+    def test1_input8(self, out):
         result, a, c = self.check_sub_menu(out)
         if result == 3:
             return '2'
         if result == 1 or result == 2:
             return CheckResult.wrong('after entering answer the sub_menu must be printed')
 
-    def sixth(self, out):
+    def test1_input9(self, out):
         result, a, b = self.check_main_menu(out)
         if result == 3:
             return '2'
         if result == 1 or result == 2:
             return CheckResult.wrong('by press no.2 it should return to main menu')
 
-    def seventh(self, out):
+    def test1_input10(self, out):
         result, error = self.check_practice_question(out, FIRST_QUESTION)
         if result == 1:
             return CheckResult.wrong(f'{error} is wrong!\nplease check extra spaces, misspelling or ":"')
@@ -182,7 +182,7 @@ class FlashCardTest(StageTest):
         if result == 4:
             return 'y'
 
-    def eighth(self, out):
+    def test1_input11(self, out):
         question = out.strip().split('\n')
         question.pop(0)
         question = '\n'.join(question)
@@ -201,7 +201,7 @@ class FlashCardTest(StageTest):
         if result == 3 and question_check == 4:
             return 'n'
 
-    def eighth_n(self, out):
+    def test1_input12(self, out):
         result, a, b = self.check_main_menu(out.strip())
         if result == 1 or result == 2:
             CheckResult.wrong('there is something wrong with main menu')
@@ -215,7 +215,7 @@ class FlashCardTest(StageTest):
             return CheckResult.wrong('Bye! is missing or misspelling!')
         return CheckResult.correct()
 
-    def first_test_two(self, out):
+    def test2_input1(self, out):
         result, error, action = self.check_main_menu(out)
         if result == 1:
             return CheckResult.wrong(f'The main menu has "3" lines and it must be like this:\n{MAIN_MENU}')
@@ -224,7 +224,7 @@ class FlashCardTest(StageTest):
         if result == 3:
             return '5'
 
-    def second_test_two(self, out):
+    def test2_input2(self, out):
         out_list = out.strip().split('\n')
         if not out_list[0] == '5 is not an option':
             return CheckResult.wrong('5 is not an option\nshould be printed')
@@ -235,7 +235,7 @@ class FlashCardTest(StageTest):
         if result == 3:
             return 'we'
 
-    def third_test_two(self, out):
+    def test2_input3(self, out):
         out_list = out.strip().split('\n')
         if not out_list[0] == 'we is not an option':
             return CheckResult.wrong('we is not an option\nshould be printed')
@@ -246,7 +246,7 @@ class FlashCardTest(StageTest):
         if result == 3:
             return CheckResult.correct()
 
-    def third_test_three(self, out):
+    def test3_input1(self, out):
         out_list = out.strip().split('\n')
         if not out_list[0] == '4 is not an option':
             return CheckResult.wrong('4 is not an option\nshould be printed')
@@ -257,7 +257,7 @@ class FlashCardTest(StageTest):
         if result == 3:
             return 'Rome'
 
-    def fourth_test_three(self, out):
+    def test3_input2(self, out):
         out_list = out.strip().split('\n')
         if not out_list[0] == 'Rome is not an option':
             return CheckResult.wrong('Rome is not an option\nshould be printed')
@@ -268,25 +268,25 @@ class FlashCardTest(StageTest):
         if result == 3:
             return CheckResult.correct()
 
-    def first_test_four(self, out):
+    def test4_input4(self, out):
         output = out.strip().split('\n')
         if not output[0] == 'Question:':
             return CheckResult.wrong("the question can't be empty!")
         return ''
 
-    def second_test_four(self, out):
+    def test4_input5(self, out):
         output = out.strip().split('\n')
         if not output[0] == 'Question:':
             return CheckResult.wrong("the question can't be empty!")
         return CheckResult.correct()
 
-    def first_test_five(self, out):
+    def test5_input5(self, out):
         output = out.strip().split('\n')
         if not output[0] == 'Answer:':
             return CheckResult.wrong("the answer can't be empty!")
         return ''
 
-    def second_test_five(self, out):
+    def test5_input6(self, out):
         output = out.strip().split('\n')
         if not output[0] == 'Answer:':
             return CheckResult.wrong("the answer can't be empty!")
